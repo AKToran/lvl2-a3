1bc41b338d3b3b68c78fec7df5edfe6a80b40664
AKToran <aktoran60@gmail.com>
2026-07-08 13:14:59 +0600

    query 1

---
+++ query.sql
@@ -0,0 +1,13 @@
+-- query 1
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  matches
+WHERE
+  tournament_category = 'Champions League'
+  AND match_status = 'Available';
+
+-- query 2
+
